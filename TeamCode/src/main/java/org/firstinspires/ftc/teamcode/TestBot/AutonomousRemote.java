c0d96e76acf161d6a68a5e27433abe12e0efad8c
esha <eshah4083@gmail.com>
2021-01-16 16:41:19 -0800

    .

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestBot/AutonomousRemote.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestBot/AutonomousRemote.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.TestBot;
+wpackage org.firstinspires.ftc.teamcode.TestBot;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
